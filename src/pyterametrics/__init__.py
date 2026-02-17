605a53fb4c874cd8791a338fff136e386c5331e4
Arihant Tripathy <arihanttr@live.com>
2026-02-17 23:03:10 +0530

    Bump version to 0.1.1 to update PyPI README

--- src/pyterametrics/__init__.py
+++ src/pyterametrics/__init__.py
@@ -1,6 +1,6 @@
 """PyTerametrics - Python port of TerraMetrics IaC quality metrics for Terraform HCL files."""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from pyterametrics.analyzer import analyze_file, analyze_directory
 from pyterametrics.metrics import collect_all_metrics
